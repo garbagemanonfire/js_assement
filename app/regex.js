034032c570558f8b4f43501fc0856dfc325d01b2
garbagemanonfire <nilesvm@gmail.com>
2013-12-01 17:22:27 -0800

    half regular expressions 9/42

--- app/regex.js
+++ app/regex.js
@@ -3,15 +3,16 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     containsNumber : function(str) {
-
+        return /\d/.test(str);
     },
 
     containsRepeatingLetter : function(str) {
+        return /([a-zA-Z])\1/.test(str);
 
     },
 
     endsWithVowel : function(str) {
-
+        return /[aeiouAEIOU]$/.test(str);
     },
 
     captureThreeNumbers : function(str) {
